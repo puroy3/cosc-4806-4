57ce4ff56803b50b5f757eee3757b82c0b7ad594
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-06-27 16:18:39 +0000

    Added CRUD functionality to reminder.php model

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -6,17 +6,36 @@ class Reminder {
 
     }
 
-    public function get_all_reminders () {
+    public function get_all_reminders ($user_id) {
       $db = db_connect();
-      $statement = $db->prepare("select * from reminders;");
+      $statement = $db->prepare("select * from notes where user_id = :user_id and deleted = 0;");
       $statement->execute();
       $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
       return $rows;
     }
 
-    public function update_reminder ($reminder_id) {
+    public function update_reminder ($id, $subject) {
       $db = db_connect();
       // do update statement
+      $statement = $db->prepare("update notes set subject = :subject where id = :id");
+      $statement->execute();
+      $rows = $statement->rowCount();
+      return $rows;
+    }
+
+    public function create_reminder ($user_id, $subject) {
+      $db = db_connect();
+      $statement = $db->prepare("insert into notes (user_id, subject, created_at) values (:user_id, :subject, current_timestamp())");
+      $statement->execute();
+      $rows = $statement->rowCount();
+      return $rows;
+    }
+    public function delete_reminder ($id) {
+      $db = db_connect();
+      $statement = $db->prepare("update notes set deleted = 1 where id = :id");
+      $statement->execute();
+      $rows = $statement->rowCount();
+      return $rows;
     }
 }
   ?>
